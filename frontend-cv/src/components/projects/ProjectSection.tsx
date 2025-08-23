769b1c6616e6f3241f1767ffec36e067a649b225
Viktoriya Nykytenko <kitakiv@gmail.com>
2025-08-23 20:57:52 +0300

    feat: add slider text to the slider start projects

--- frontend-cv/src/components/projects/ProjectSection.tsx
+++ frontend-cv/src/components/projects/ProjectSection.tsx
@@ -1,12 +1,14 @@
 import TextGray from "@/components/text/TextGray"
 import TextHeading from "@/components/text/TextHeading"
 import projects from "@/variables/projects/projects"
+import CardProject from "./components/CardProject"
 
 export default function ProjectSection() {
     return (
         <section className="flex flex-col items-center justify-center lg:py-16 md:py-14 sm:py-12 py-10 lg:px-6 md:px-6 sm:px-4 px-2 gap-4" id={projects.id}>
             <TextHeading>{projects.heading}</TextHeading>
             <TextGray>{projects.text}</TextGray>
+            <CardProject />
         </section>
     )
 }
