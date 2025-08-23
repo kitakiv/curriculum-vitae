73761fceb6310e7ef147ff04d179dc3810579975
Viktoriya Nykytenko <kitakiv@gmail.com>
2025-08-23 14:52:36 +0300

    feat: add slider

--- frontend-cv/src/components/aboutme/components/Slider.tsx
+++ frontend-cv/src/components/aboutme/components/Slider.tsx
@@ -1,9 +1,10 @@
 "use client"
 import {useState} from "react"
 import Image from "next/image"
-export default function Slider() {
+
+const imagesDefault = ["/image/developer1.jpg", "/image/developer2.jpg", "/image/developer3.jpg"];
+export default function Slider({images = imagesDefault}: {images: string[]}) {
     const [mainImage, setMainImage] = useState(0);
-    const [tailwind, setTailwind] = useState("");
 
     function changeLeft() {
         setMainImage((prev) => {
@@ -25,12 +26,28 @@ export default function Slider() {
         })
 
     }
-
-    const images = ["/image/developer1.jpg", "/image/developer2.jpg", "/image/developer3.jpg"];
-    return (<div className={`lg:w-2/3 sm:w-4/5 w-5/6 h-96 relative overflow-hidden flex  justify-end`}>
+    return (<div className={`h-96 lg:w-2/3 sm:w-4/5 w-5/6 relative overflow-hidden flex bg-gradient-to-r from-txSecond to-bg0`}>
         <div className="absolute top-1/2 left-3  -translate-y-1/2 w-11 h-11 bg-bg0 rounded-full z-50 transition duration-700 border-light border-2 hover:scale-110"  onClick={changeLeft}></div>
         <div className="absolute top-1/2 right-3 -translate-y-1/2 w-11 h-11 bg-bg0 rounded-full z-50 transition duration-700 border-light border-2 hover:scale-110 " onClick={changeRight}></div>
-        <div className="z-20 w-full h-full absolute top-0 left-0 bg-gradient-to-r from-bg0 from-0% via-bg100 lg:via-65%  md:via-55% sm:via-45% via-35% to-bg100-0 lg:to-85% md:to-75% sm:to-65% to-55%"></div>
-        <Image src={images[mainImage]} alt="developer" width={384} height={384} className={`absolute top-0 right-0 h-full w-fit transition-transform duration-1000 translate-x-0 ${tailwind}`}></Image>
+        <div className={`transition duration-700 flex w-full h-full -translate-x-[${mainImage * 100}%]`}>
+        {images.map((image, index) => {
+            return (
+                <div key={`image-slider-${index}`} className="flex-shrink-0 w-[100%] h-full relative">
+                    <Image src={image} alt="developer" width={300} height={300} className={`image-mask transition duration-700  w-fit h-full absolute right-0 top-0`} />
+                </div>
+            )
+        })}
+        </div>
+        <div className="absolute bottom-3 left-1/2 -translate-x-1/2 z-50 flex gap-1">
+            {images.map((image, index) => {
+                return (
+                    <div className="lg:w-9 lg:h-9 md:w-11 md:h-11 sm:w-20 sm:h-20 w-20 h-20 flex items-center justify-center group" key={`image-slider-dot-${index}`} onClick={() => setMainImage(index)}>
+                        <div className={`w-3 h-3 rounded-full transition duration-700 ${mainImage === index ? "bg-gradient-to-br from-txFirst100 to-txFirst0" : "bg-light  group-hover:scale-125 cursor-pointer group-hover:bg-txFirst0"}`}></div>
+                    </div>
+                )
+            })}
+
+        </div>
     </div>)
+
 }
