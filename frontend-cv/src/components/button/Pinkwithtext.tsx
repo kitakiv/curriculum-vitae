bbcc9523063fa5707c93c835bb43129fed2d204f
Viktoriya Nykytenko <kitakiv@gmail.com>
2025-09-25 18:56:08 +0300

    feat: edit buttons

--- frontend-cv/src/components/button/Pinkwithtext.tsx
+++
@@ -1,9 +0,0 @@
-import 
-export default function PinkWithText({children, tailwind}: {children: React.ReactNode, tailwind?: string}) {
-    return (
-        <PinkButton type="submit" tailwind="transition duration-700 group flex justify-center items-center gap-2 hover:shadow-lg hover:shadow-txSecond">
-    //                 {form.loginForm.buttonText}
-    //                 <Image src={header.arrow} alt="arrow" width={20} height={20} className="w-0 opacity-0 group-hover:w-5 group-hover:opacity-100 transition duration-700"></Image>
-    //       </PinkButton>
-    )
-}
